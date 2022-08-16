7a29928f9ea7ca06d8311a4bf19e8c658a1a404f
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-16 18:05:59 +0600

    import: header component

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Box, CssBaseline } from "@mui/material";
+import Header from "components/Header";
+import React from "react";
 
 const Root = () => {
   return (
-    <div>Root</div>
-  )
-}
+    <>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
+        <Box>
+          <Header />
+        </Box>
+      </Box>
+    </>
+  );
+};
 
-export default Root
+export default Root;
