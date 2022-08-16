0aa1d822942fd3a6e02db6a4110927a683754863
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-16 18:06:12 +0600

    build: theme provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "@mui/material";
 import Root from "./Root";
-
+import { theme } from "./themes/theme";
 function App() {
-  return <Root />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Root />
+    </ThemeProvider>
+  );
 }
 
 export default App;
