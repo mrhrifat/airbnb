2c869eb4bf198073be489ca28d4a979f9c1d391a
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-17 15:58:32 +0600

    feat: build footer (#3)

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,7 +1,12 @@
 import { Box, CssBaseline } from "@mui/material";
+import Footer from "components/Footer";
+import FooterMenu from "components/FooterMenu";
 import Header from "components/Header";
+import LocationCard from "components/LocationCard";
+import MobileFooter from "components/MobileFooter";
 import OptionsTab from "components/OptionTabs";
 import React from "react";
+import { displayOnDesktop } from "themes/commonStyles";
 
 const Root = () => {
   return (
@@ -17,6 +22,20 @@ const Root = () => {
         <Box>
           <Header />
           <OptionsTab />
+          <LocationCard />
+          <Box
+            sx={{
+              display: { xs: "flex", md: "none" },
+            }}
+          >
+            <MobileFooter />
+          </Box>
+        </Box>
+        <Box sx={{ display: { xs: "flex", md: "none" } }}>
+          <FooterMenu />
+        </Box>
+        <Box sx={displayOnDesktop}>
+          <Footer />
         </Box>
       </Box>
     </>
