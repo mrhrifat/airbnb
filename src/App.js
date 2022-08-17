4f88f4e76af31ddb5c01e01d5e03726e7f5f5337
Mrh Rifat <mrhrifat383759@gmail.com>
2022-08-17 11:21:29 +0600

    reactor: change name of theme file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "@mui/material";
 import Root from "./Root";
-import { theme } from "./themes/theme";
+import { theme } from "./themes/customTheme";
 function App() {
   return (
     <ThemeProvider theme={theme}>
